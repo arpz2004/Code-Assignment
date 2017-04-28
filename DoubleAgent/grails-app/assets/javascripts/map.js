957b50a9e4f12e6684b958e78a99a01737c8c0dc
Adrien Lindner <lindner.54@osu.edu>
2017-04-28 14:13:50 -0400

    All spies how up on map in correct location.

--- DoubleAgent/grails-app/assets/javascripts/map.js
+++ DoubleAgent/grails-app/assets/javascripts/map.js
@@ -1,16 +1,33 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXJwejIwMDQiLCJhIjoiY2oyMWsyeWR5MDAybzJ2cDIwMGg3dXY4eiJ9.7xlHSnQ2tf9H_ZRKFokBNg', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
-}).addTo(mymap);
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.marker([51.5, -0.09]).addTo(mymap)
-    .bindPopup('test')
-    .openPopup();
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXJwejIwMDQiLCJhIjoiY2oyMWsyeWR5MDAybzJ2cDIwMGg3dXY4eiJ9.7xlHSnQ2tf9H_ZRKFokBNg', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18
+    }).addTo(mymap);
 
-function addToMap(lat, long){
-    L.marker([lat, long]).addTo(mymap)
-        .bindPopup('test')
+    L.marker([51.51, -0.09]).addTo(mymap)
+        .bindPopup('test1')
         .openPopup();
+    test3()
+function test2() {
+        return function() {
+            L.marker([51.49, -0.09]).addTo(mymap)
+                .bindPopup('test2')
+                .openPopup();
+        }
+}
+
+function test3() {
+    L.marker([51.5, -0.09]).addTo(mymap)
+        .bindPopup('test3')
+        .openPopup();
+}
+
+function addToMap(lat, long) {
+    return function () {
+        L.marker([lat, long]).addTo(mymap)
+            .bindPopup('test4')
+            .openPopup();
+    }
 }
