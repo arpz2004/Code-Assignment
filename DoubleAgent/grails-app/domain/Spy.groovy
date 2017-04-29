568886fce42baafcefe1904d22e7eb046e717848
Adrien Lindner <lindner.54@osu.edu>
2017-04-29 12:59:24 -0400

    Added more validations

--- DoubleAgent/grails-app/domain/Spy.groovy
+++ DoubleAgent/grails-app/domain/Spy.groovy
@@ -6,6 +6,10 @@ class Spy{
     String gender
 
     static constraints = {
-
+        name blank: false
+        longitude blank: false, min: -180f, max: 180f
+        latitude blank: false, min: -90f, max: 90f
+        age blank: false, min: 0, max: 200
+        gender blank: false
     }
 }
